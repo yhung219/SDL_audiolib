562cc359fe2fa95292a3953a04ed821fa02bc9c4
Nikos Chantziaras <realnc@gmail.com>
2016-02-08 02:22:12 +0200

    Doc update

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['init',['init',['../class_aulib_1_1_audio_decoder_wildmidi.html#abcc5bffcd99a9e6499a75016e15ff28d',1,'Aulib::AudioDecoderWildmidi::init()'],['../namespace_aulib.html#ac82bf0fbde45439052c1ffbca293d668',1,'Aulib::init()']]],
+  ['init',['init',['../class_aulib_1_1_audio_decoder_wildmidi.html#abcc5bffcd99a9e6499a75016e15ff28d',1,'Aulib::AudioDecoderWildmidi::init()'],['../namespace_aulib.html#ad3c742f54c43b0c381a1ed8a44c79b1d',1,'Aulib::init()']]],
   ['invokefinishcallback',['invokeFinishCallback',['../class_aulib_1_1_stream.html#a9a55fce0144fb7ad9945afd2da7a207d',1,'Aulib::Stream']]],
   ['invokeloopcallback',['invokeLoopCallback',['../class_aulib_1_1_stream.html#a10d6c126a098c7adcdbac797352326e2',1,'Aulib::Stream']]],
   ['isopen',['isOpen',['../class_aulib_1_1_audio_decoder.html#aa7de4e439fa3d1d64c94b022f86c30a4',1,'Aulib::AudioDecoder']]],
